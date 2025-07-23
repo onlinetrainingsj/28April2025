6e2be709f6e40810ca9771a86c526a7c613f2956
Shammi_Jha <Shammi_Jha@DESKTOP-17LHQBD>
2025-07-23 22:17:14 +0530

    Adding project to github repo

---
+++ TestApril2025Project/src/test/java/javaPackage/Test1.java
@@ -0,0 +1,19 @@
+package javaPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	@Test
+	public void a() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://dashboard.gotowebinar.com/contentLibrary");
+		System.out.println("Hello Git");
+	}
+
+}
